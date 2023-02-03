4e16eb7f80b6043dab600470db0200e95d98d646
João Pedro <112526011+JoaoPedro963@users.noreply.github.com>
2023-02-03 20:12:39 -0300

    Adicionar arquivos de projeto.

---
+++ Calculos CSharp/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Calculos_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPrimeiroNumero = new System.Windows.Forms.TextBox();
+            this.txtSegundoNumero = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("OCR-A BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Primeiro Número";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtPrimeiroNumero
+            // 
+            this.txtPrimeiroNumero.BackColor = System.Drawing.Color.Silver;
+            this.txtPrimeiroNumero.Font = new System.Drawing.Font("OCR-A BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrimeiroNumero.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtPrimeiroNumero.Location = new System.Drawing.Point(15, 36);
+            this.txtPrimeiroNumero.Name = "txtPrimeiroNumero";
+            this.txtPrimeiroNumero.Size = new System.Drawing.Size(100, 22);
+            this.txtPrimeiroNumero.TabIndex = 1;
+            // 
+            // txtSegundoNumero
+            // 
+            this.txtSegundoNumero.BackColor = System.Drawing.Color.Silver;
+            this.txtSegundoNumero.Font = new System.Drawing.Font("OCR-A BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSegundoNumero.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtSegundoNumero.Location = new System.Drawing.Point(157, 36);
+            this.txtSegundoNumero.Name = "txtSegundoNumero";
+            this.txtSegundoNumero.Size = new System.Drawing.Size(100, 22);
+            this.txtSegundoNumero.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("OCR-A BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(154, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Segundo Número";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.Color.Silver;
+            this.txtResultado.Font = new System.Drawing.Font("OCR-A BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtResultado.Location = new System.Drawing.Point(15, 85);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(136, 22);
+            this.txtResultado.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("OCR-A BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 14);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Resultado";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.Teal;
+            this.btnCalcular.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnCalcular.FlatAppearance.BorderSize = 3;
+            this.btnCalcular.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnCalcular.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Turquoise;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("OCR-A BT", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCalcular.Location = new System.Drawing.Point(157, 69);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 39);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("OCR-A BT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(128, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "-";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(284, 131);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSegundoNumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPrimeiroNumero);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPrimeiroNumero;
+        private System.Windows.Forms.TextBox txtSegundoNumero;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Calculos CSharp/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculos_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int valor1 = Convert.ToInt32(txtPrimeiroNumero.Text);
+            int valor2 = Convert.ToInt32(txtSegundoNumero.Text);
+            int resposta = valor1 - valor2;
+            txtResultado.Text = resposta.ToString();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
